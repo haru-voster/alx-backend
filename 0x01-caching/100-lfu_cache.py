03c63732fab54a71b0bf3205ddf8f998e1faee38
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2024-01-31 12:39:17 +0200

    Update 100-lfu_cache.py

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -9,7 +9,8 @@ class LFUCache(BaseCaching):
     ''' An LFU cache.
         Inherits all behaviors from BaseCaching except, upon any attempt to add
         an entry to the cache when it is at max capacity (as specified by
-        BaseCaching.MAX_ITEMS).
+        BaseCaching.MAX_ITEMS), it discards the least frequently used entry to
+        accommodate for the new one.
         Attributes:
           __init__ - method that initializes class instance
           put - method that adds a key/value pair to cache
@@ -23,8 +24,8 @@ class LFUCache(BaseCaching):
 
     def put(self, key, item):
         ''' Add key/value pair to cache data.
-            If cache is at max capacity (specified by BaseCaching.MAX_ITEMS). 
-        '''
+            If cache is at max capacity (specified by BaseCaching.MAX_ITEMS),
+            discard least frequently used entry to accommodate new entry. '''
 
         if key is not None and item is not None:
             if (len(self.keys) == BaseCaching.MAX_ITEMS and
